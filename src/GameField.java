faec838b267446acc4d11240493a2341138e19ae
jeas4378 <jeas4378@student.su.se>
2021-03-15 17:18:00 +0100

    Grafik paddles klar

--- src/GameField.java
+++ src/GameField.java
@@ -7,6 +7,10 @@ import java.util.Map;
 public class GameField extends JPanel {
 
     private HashMap<Point,Color> lines = new HashMap<Point,Color>();
+    private HashMap<Point,Color> p1 = new HashMap<Point,Color>();
+    private HashMap<Point,Color> p2 = new HashMap<Point,Color>();
+    private int p1_position = 0;
+    private int p2_position = 0;
 
     public GameField() {
 
@@ -18,6 +22,8 @@ public class GameField extends JPanel {
 
         Boolean block = false;
         Boolean middle = false;
+        int height_min = 220;
+        int height_max = 280;
 
         for (int y = 0; y < 501; y++) {
             for (int x = 0; x < 1001; x++) {
@@ -28,7 +34,12 @@ public class GameField extends JPanel {
                 else {
                     lines.put(p,Color.WHITE);
                 }
-
+                if ((x > 29) && (x < 40) && (y > height_min) && (y < height_max)) {
+                    p1.put(p, Color.BLACK);
+                }
+                if ((x > 764) && (x < 776) && (y > height_min) && (y < height_max)) {
+                    p2.put(p, Color.BLACK);
+                }
                 if ((y < 20) || (y > 460)) {
                     block = true;
                 }
@@ -68,5 +79,43 @@ public class GameField extends JPanel {
             g.setColor(c);
             g.fillRect(p.x, p.y, 1, 1);
         }
+
+        Iterator player1 = p1.entrySet().iterator();
+
+        while(player1.hasNext()) {
+            Map.Entry pair_p1 = (Map.Entry) player1.next();
+            Point p1_pos = (Point)pair_p1.getKey();
+            Color p1_col = (Color)pair_p1.getValue();
+
+            g.setColor(p1_col);
+            g.fillRect(p1_pos.x + getP1_position(),p1_pos.y,1,1);
+        }
+
+        Iterator player2 = p2.entrySet().iterator();
+
+        while(player2.hasNext()) {
+            Map.Entry pair_p2 = (Map.Entry) player2.next();
+            Point p2_pos = (Point)pair_p2.getKey();
+            Color p2_col = (Color)pair_p2.getValue();
+
+            g.setColor(p2_col);
+            g.fillRect(p2_pos.x + getP2_position(),p2_pos.y,1,1);
+        }
+    }
+
+    public void setP1(int pos) {
+        this.p1_position = pos;
+    }
+
+    public void setP2(int pos) {
+        this.p2_position = pos;
+    }
+
+    public int getP1_position() {
+        return this.p1_position;
+    }
+
+    public int getP2_position(){
+        return this.p2_position;
     }
 }
