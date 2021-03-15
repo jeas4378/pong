bff1007371b70dfbbc1dcf29a823da45ec468199
jeas4378 <jeas4378@student.su.se>
2021-03-15 21:10:59 +0100

    Påbörjar klient-nätverk

---
+++ src/ClientConnection.java
@@ -0,0 +1,10 @@
+public class ClientConnection implements Runnable{
+
+    private static final int DEFAULT_PORT = 2000;
+    private static final String DEFAULT_SERVER = "127.0.0.1";
+
+    @Override
+    public void run() {
+        
+    }
+}
